c14980a6c48063acd70688f69398562cef3aae1e
Ethan Rowley <ethanrowley7@icloud.com>
2024-02-08 22:33:51 +0000

    initial project setup

---
+++ Backend/src/main/java/com/rowley/PhysicsQuiz/PhysicsQuizApplication.java
@@ -0,0 +1,14 @@
+package com.rowley.PhysicsQuiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhysicsQuizApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		SpringApplication.run(PhysicsQuizApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/rowley/PhysicsQuiz/PhysicsQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rowley.PhysicsQuiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhysicsQuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
